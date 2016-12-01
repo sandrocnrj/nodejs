28bc19c7003968eabcf73a2bfa560b536f1a19fc
Sandro Carvalho <sandro.prog@gmail.com>
2016-12-01 12:04:56 -0200

    Abrindo uma conexao com o MySQL

--- projeto/app/routes/produtos.js
+++ projeto/app/routes/produtos.js
@@ -1,5 +1,20 @@
 module.exports = function(app){
     app.get('/produtos', function(req,res){
-        res.render("produtos/lista"); 
+    	var mysql = require('mysql');
+    	var connection = mysql.createConnection({
+    		host : 'localhost',
+    		user : 'root',
+    		password : 'mul4mb02016',
+    		database : 'prjnodejs'
+    	});
+
+    	connection.query('select * from livros', function(err, results){
+    		res.send(results);
+    	});
+
+    	connection.end();
+
+    	//consulta
+        //res.render("produtos/lista"); 
     });
 }
