2f40a641ad5ba09aac1310c3464fc3b11c395187
Sandro Carvalho <sandro.prog@gmail.com>
2016-11-30 22:31:16 -0200

    Testando redirecionamentos

--- produtos.js
+++ produtos.js
@@ -1,1 +1,11 @@
-console.log("listagem de produtos")
+var http = require('http');
+
+http.createServer(function(req, res){
+	if(req.url == "/produtos"){
+		res.end("<html><body>listando os produtos da loja</body>");
+	} else {
+		res.end("<html><body>home  da casa do codigo</body></html>");
+	}
+}).listen(3000,"localhost");
+
+console.log("listagem de produtos");
