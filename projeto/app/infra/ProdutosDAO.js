7ad349fd11b406147a045cff6f6dd59983371b70
Sandro Carvalho <sandro.prog@gmail.com>
2016-12-05 21:35:58 -0200

    Modulo de Body-Parser

--- projeto/app/infra/ProdutosDAO.js
+++ projeto/app/infra/ProdutosDAO.js
@@ -8,6 +8,11 @@ ProdutosDAO.prototype.lista = function(callback) {
 }
 
 
+ProdutosDAO.prototype.salva = function(produto,callback) {
+	this._connection.query('insert into livros set ?', produto, callback);
+}
+
+
 module.exports = function() {
 	return ProdutosDAO;
 }
--- projeto/app/routes/produtos.js
+++ projeto/app/routes/produtos.js
@@ -1,17 +1,30 @@
 module.exports = function(app){
-    app.get('/produtos', function(req,res){
-    	var connection =   app.infra.connectionFactory();
-    	var produtosBanco = new app.infra.ProdutosDAO(connection);
+    var listaProdutos = function(req,res){
+        var connection =   app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
 
-    	produtosBanco.lista(function(err, results){
-    		res.render('produtos/lista', {lista:results});
-    	});
+        produtosDAO.lista(function(err, results){
+            res.render('produtos/lista', {lista:results});
+        });
 
-    	connection.end();
-    });
+        connection.end();
+    };
+
+    app.get('/produtos', listaProdutos);
 
     app.get('/produtos/form', function(req, res){
     	res.render('produtos/form');
 
+    });
+
+    app.post('/produtos', function(req, res){
+        var produto = req.body;
+
+        var connection =   app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);        
+
+        produtosDAO.salva(produto, function(erros, results){
+            res.redirect('/produtos')
+        });        
     });
 }
