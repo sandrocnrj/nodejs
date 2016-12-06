77127a68e890a83d373740b2e0b4210cbd66c63a
Sandro Carvalho <sandro.prog@gmail.com>
2016-12-06 11:30:15 -0200

    Melhorando a validação

--- projeto/app/routes/produtos.js
+++ projeto/app/routes/produtos.js
@@ -35,20 +35,23 @@ module.exports = function(app){
     app.get('/produtos', listaProdutos);
 
     app.get('/produtos/form', function(req, res){
-    	res.render('produtos/form');
+    	res.render('produtos/form',{errosValidacao:{}});
 
     });
 
     app.post('/produtos', function(req, res){
         var produto = req.body;
         
-        var validadorTItulo = req.assert('titulo', 'Titulo é obrigatório');
-        validadorTItulo.notEmpty();
+        //var validadorTItulo = req.assert('titulo', 'Titulo é obrigatório');
+        //validadorTItulo.notEmpty();
+
+        req.assert('titulo', 'Titulo é obrigatório').notEmpty();
+        req.assert('preco', 'Formato inválido').isFloat();
 
         var erros = req.validationErrors();
 
         if(erros){
-            res.render('produtos/form');
+            res.render('produtos/form',{errosValidacao:erros});
             return;
         }
 
